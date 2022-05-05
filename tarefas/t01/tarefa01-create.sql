3f931a159fec5197b7663a0aa514ef3c94bc7a87
Douglas Mateus <douglasmateus1@hotmail.com>
2022-05-05 14:37:18 -0300

    Editando o script tarefa-01-create.sql. #1

--- tarefas/t01/tarefa01-create.sql
+++ tarefas/t01/tarefa01-create.sql
@@ -1,1 +1,23 @@
+drop table if exists postgres.public.funcionario cascade;
+drop table if exists postgres.public.departamento cascade;
 
+create table if not exists postgres.public.departamento
+(
+	codigo serial primary key,
+	descricao varchar(100) not null,
+	cod_gerente int default null
+);
+
+alter table postgres.public.departamento add foreign key (cod_gerente) references funcionario(codigo) on update cascade on delete set null ;
+
+create table if not exists postgres.public.funcionario
+(
+	codigo int primary key not null,
+	nome varchar(100) not null,
+	sexo char(1) not null,
+	dt_nasc date not null,
+	salario decimal(10, 2) not null,
+	cod_depto int default null
+);
+
+alter table postgres.public.funcionario add foreign key(cod_depto) references departamento(codigo) on update cascade on delete restrict;
