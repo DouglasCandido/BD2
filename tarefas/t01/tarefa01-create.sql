4401ccbc91bf752071e1ab4a644b40d7bc8c8f78
Douglas Cândido <douglasmateus1@hotmail.com>
2022-05-07 12:27:04 -0300

    enhancement: Script de criação concluído. #1

--- tarefas/t01/tarefa01-create.sql
+++ tarefas/t01/tarefa01-create.sql
@@ -2,6 +2,9 @@
 
 drop table if exists postgres.public.funcionario cascade;
 drop table if exists postgres.public.departamento cascade;
+drop table if exists postgres.public.projeto cascade;
+drop table if exists postgres.public.atividade cascade;
+drop table if exists postgres.public.atividade_projeto cascade;
 
 /* ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ */
 
@@ -35,6 +38,24 @@ create table if not exists postgres.public.projeto
 	data_fim date default null
 );
 
+create table if not exists postgres.public.atividade
+(
+	codigo serial primary key,
+	nome varchar(100) not null,
+	descricao varchar(100) not null,
+	cod_responsavel int not null,
+	data_inicio date not null,
+	data_fim date default null
+);
+
+create table if not exists postgres.public.atividade_projeto
+(
+	codigo serial, /* Este atributo Codigo serve apenas para organização */
+	cod_projeto int not null,
+	cod_atividade int not null,
+	constraint pk_atividade_projeto primary key(cod_projeto, cod_atividade)
+);
+
 /* ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ */
 
 /* Foreign Keys */
@@ -44,7 +65,12 @@ alter table postgres.public.funcionario add foreign key(cod_depto) references de
 alter table postgres.public.departamento add foreign key(cod_gerente) references funcionario(codigo) on update cascade on delete restrict;
 
 alter table postgres.public.projeto add foreign key(cod_depto) references departamento(codigo) on update cascade on delete restrict;
-alter table postgres.public.projeto add foreign key(cod_responsavel) references funcionario(codigo) on update cascade on delete restrict;
+alter table postgres.public.projeto add foreign key(cod_responsavel) references funcionario(codigo) on update cascade on delete set null;
+
+alter table postgres.public.atividade add foreign key(cod_responsavel) references funcionario(codigo) on update cascade  on delete set null;
+
+alter table postgres.public.atividade_projeto add foreign key(cod_projeto) references projeto(codigo) on update cascade on delete set null;
+alter table postgres.public.atividade_projeto add foreign key(cod_atividade) references atividade(codigo) on update cascade on delete set null;
 
 /* ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ */
 
