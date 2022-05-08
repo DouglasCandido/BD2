0a4a4294330f4c249346f444a0a899265cbebfac
Douglas Cândido <douglasmateus1@hotmail.com>
2022-05-08 11:38:12 -0300

    enhancement: Inserts Tabela projeto. #1

--- tarefas/t01/tarefa01-inserts.sql
+++ tarefas/t01/tarefa01-inserts.sql
@@ -22,7 +22,11 @@ insert into departamento(descricao, cod_gerente) values('Departamento de Ciênci
 
 /* Tabela projeto */
 
-insert into projeto() values();
+insert into projeto(nome, descricao, cod_depto, cod_responsavel, data_inicio, data_fim) values('Projeto de Pesquisa & Desenvolvimento', 'Formular estratégias visando a inovação por meio da criação de novos produtos e patentes', 1, 1, '2022-05-08');
+insert into projeto(nome, descricao, cod_depto, cod_responsavel, data_inicio, data_fim) values('Projeto de criação de novos canais de comunicação', 'Buscar novos meios de comunicação com a sociedade para realizar o Marketing', 2, 2, '2022-05-08');
+insert into projeto(nome, descricao, cod_depto, cod_responsavel, data_inicio, data_fim) values('Projeto de Capacitação dos Funcionários', 'Ofertar cursos atuais sobre os conhecimentos atuais de cada área para capacitar os funcionários de cada departamento', 3, 3, '2022-05-08');
+insert into projeto(nome, descricao, cod_depto, cod_responsavel, data_inicio, data_fim) values('Projeto de Redesign da Infraestrutura de Rede da empresa', 'Redesenhar e implementar uma nova infraestrutura de rede na empresa, buscando a máxima eficiência', 4, 4, '2022-05-08');
+insert into projeto(nome, descricao, cod_depto, cod_responsavel, data_inicio, data_fim) values('Projeto de Implementação de uso da Energia Limpa e Renovável na empresa', 'Instalar paineis fotovoltaicos com o objetivo de buscar a economia da energia utilizada pela empresa', 5, 5, '2022-05-08');
 
 /* ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ */
 
