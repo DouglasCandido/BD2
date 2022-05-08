72112215e431dff11cdec5850b14de745fc28fdd
Douglas Cândido <douglasmateus1@hotmail.com>
2022-05-08 11:12:12 -0300

    enhancement: Inserts Tabela Funcionario #1

---
+++ tarefas/t01/tarefa01-inserts.sql
@@ -0,0 +1,19 @@
+/* Povoamento inicial */
+
+/* Tabela Funcionario */
+
+insert into funcionario(nome, sexo, dt_nasc, salario, cod_depto) values('Douglas Mateus Soares Cândido da Silva', 'H', '1997-09-23', 3333.33, 1);
+insert into funcionario(nome, sexo, dt_nasc, salario, cod_depto) values('Saint-Clair Cândido da Silva', 'H', '1971-09-07', 3333.33, 1);
+insert into funcionario(nome, sexo, dt_nasc, salario, cod_depto) values('Erivonete Soares de Lucena', 'M', '1969-05-10', 3333.33, 1);
+insert into funcionario(nome, sexo, dt_nasc, salario, cod_depto) values('Steven Paul Jobs', 'H', '1955-02-24', 100000.00, 2);
+insert into funcionario(nome, sexo, dt_nasc, salario, cod_depto) values('Albert Einstein', 'H', '1879-03-14', 50000.00, 2);
+
+/* ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ */
+
+insert into departamento() values();
+
+insert into projeto() values();
+
+insert into atividade() values();
+
+insert into atividade_projeto() values();
