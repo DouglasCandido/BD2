25643f51daaab2877ac6612e5fce5ae07a93076d
Douglas Cândido <douglasmateus1@hotmail.com>
2022-05-08 12:22:00 -0300

    enhancement: Inserts Tabela atividade. #1

--- tarefas/t01/tarefa01-inserts.sql
+++ tarefas/t01/tarefa01-inserts.sql
@@ -22,17 +22,22 @@ insert into departamento(descricao, cod_gerente) values('Departamento de Ciênci
 
 /* Tabela projeto */
 
-insert into projeto(nome, descricao, cod_depto, cod_responsavel, data_inicio, data_fim) values('Projeto de Pesquisa & Desenvolvimento', 'Formular estratégias visando a inovação por meio da criação de novos produtos e patentes', 1, 1, '2022-05-08');
-insert into projeto(nome, descricao, cod_depto, cod_responsavel, data_inicio, data_fim) values('Projeto de criação de novos canais de comunicação', 'Buscar novos meios de comunicação com a sociedade para realizar o Marketing', 2, 2, '2022-05-08');
-insert into projeto(nome, descricao, cod_depto, cod_responsavel, data_inicio, data_fim) values('Projeto de Capacitação dos Funcionários', 'Ofertar cursos atuais sobre os conhecimentos atuais de cada área para capacitar os funcionários de cada departamento', 3, 3, '2022-05-08');
-insert into projeto(nome, descricao, cod_depto, cod_responsavel, data_inicio, data_fim) values('Projeto de Redesign da Infraestrutura de Rede da empresa', 'Redesenhar e implementar uma nova infraestrutura de rede na empresa, buscando a máxima eficiência', 4, 4, '2022-05-08');
-insert into projeto(nome, descricao, cod_depto, cod_responsavel, data_inicio, data_fim) values('Projeto de Implementação de uso da Energia Limpa e Renovável na empresa', 'Instalar paineis fotovoltaicos com o objetivo de buscar a economia da energia utilizada pela empresa', 5, 5, '2022-05-08');
+insert into projeto(nome, descricao, cod_depto, cod_responsavel, data_inicio) values('Projeto de Pesquisa & Desenvolvimento', 'Formular estratégias visando a inovação por meio da criação de novos produtos e patentes', 1, 1, '2022-05-08');
+insert into projeto(nome, descricao, cod_depto, cod_responsavel, data_inicio) values('Projeto de criação de novos canais de comunicação', 'Buscar novos meios de comunicação com a sociedade para realizar o Marketing', 2, 2, '2022-05-08');
+insert into projeto(nome, descricao, cod_depto, cod_responsavel, data_inicio) values('Projeto de Capacitação dos Funcionários', 'Ofertar cursos atuais sobre os conhecimentos atuais de cada área para capacitar os funcionários de cada departamento', 3, 3, '2022-05-08');
+insert into projeto(nome, descricao, cod_depto, cod_responsavel, data_inicio) values('Projeto de Redesign da Infraestrutura de Rede da empresa', 'Redesenhar e implementar uma nova infraestrutura de rede na empresa, buscando a máxima eficiência', 4, 4, '2022-05-08');
+insert into projeto(nome, descricao, cod_depto, cod_responsavel, data_inicio) values('Projeto de Implementação de uso da Energia Limpa e Renovável na empresa', 'Instalar paineis fotovoltaicos com o objetivo de buscar a economia da energia utilizada pela empresa', 5, 5, '2022-05-08');
+
 
 /* ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ */
 
 /* Tabela atividade */
 
-insert into atividade() values();
+insert into atividade(nome, descricao, cod_responsavel, data_inicio) values('Brainstorm de ideias', 'Realizar uma atividade de brainstorm para gerar novas ideias de novos produtos', 1, '2022-05-08');
+insert into atividade(nome, descricao, cod_responsavel, data_inicio) values('Análise do uso dos canais de comunicação pela população', 'Pesquisar indicadores e métricas estatísticas a respeito do uso de cada canal de comunicação', 2, '2022-05-08');
+insert into atividade(nome, descricao, cod_responsavel, data_inicio) values('Contratação de professores capacitados para ofertar os cursos necessários', 'Buscar professores que dominam os temas mais atuais de cada área do conhecimento e contrata-los para ministrar os cursos', 3, '2022-05-08');
+insert into atividade(nome, descricao, cod_responsavel, data_inicio) values('Projeto e Implementação da Topologia de Rede', 'Realizar o design da topologia de rede que será aplicada à nova infraestrutura', 4, '2022-05-08');
+insert into atividade(nome, descricao, cod_responsavel, data_inicio) values('Elaborar um plano de implementação incremental para as novas fontes de energia', 'Instalar e configurar gradativamente os paineis fotovoltaicos', 5, '2022-05-08');
 
 /* ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ */
 
