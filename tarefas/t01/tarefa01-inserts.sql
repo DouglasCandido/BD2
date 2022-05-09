df017b8e8d98185ad5cfaf7134f172047391a3f2
Douglas Cândido <douglasmateus1@hotmail.com>
2022-05-09 11:05:55 -0300

    enhancement: Criado Script SQL que resolve a Questão 1 da lista. #1

--- tarefas/t01/tarefa01-inserts.sql
+++ tarefas/t01/tarefa01-inserts.sql
@@ -3,10 +3,11 @@
 /* Tabela funcionario */
 
 insert into funcionario(nome, sexo, dt_nasc, salario, cod_depto) values('Douglas Mateus Soares Cândido da Silva', 'H', '1997-09-23', 3333.33, 1);
-insert into funcionario(nome, sexo, dt_nasc, salario, cod_depto) values('Saint-Clair Cândido da Silva', 'H', '1971-09-07', 3333.33, 1);
-insert into funcionario(nome, sexo, dt_nasc, salario, cod_depto) values('Erivonete Soares de Lucena', 'M', '1969-05-10', 3333.33, 1);
-insert into funcionario(nome, sexo, dt_nasc, salario, cod_depto) values('Steven Paul Jobs', 'H', '1955-02-24', 100000.00, 2);
-insert into funcionario(nome, sexo, dt_nasc, salario, cod_depto) values('Albert Einstein', 'H', '1879-03-14', 50000.00, 2);
+insert into funcionario(nome, sexo, dt_nasc, salario, cod_depto) values('Saint-Clair Cândido da Silva', 'H', '1971-09-07', 3333.33, 2);
+insert into funcionario(nome, sexo, dt_nasc, salario, cod_depto) values('Erivonete Soares de Lucena', 'M', '1969-05-10', 3333.33, 3);
+insert into funcionario(nome, sexo, dt_nasc, salario, cod_depto) values('Steven Paul Jobs', 'H', '1955-02-24', 100000.00, 4);
+insert into funcionario(nome, sexo, dt_nasc, salario, cod_depto) values('Bruce Wayne', 'H', '1915-04-17', '150000', 4);
+insert into funcionario(nome, sexo, dt_nasc, salario, cod_depto) values('Albert Einstein', 'H', '1879-03-14', 50000.00, 5);
 
 /* ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ */
 
