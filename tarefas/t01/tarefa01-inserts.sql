7ae8b275be2f0dd3677878477a8c3eaea6bf6c53
Douglas Cândido <douglasmateus1@hotmail.com>
2022-05-09 12:49:57 -0300

    enhancement: Criado script que resolve a questão 3. #1

--- tarefas/t01/tarefa01-inserts.sql
+++ tarefas/t01/tarefa01-inserts.sql
@@ -3,6 +3,7 @@
 /* Tabela funcionario */
 
 insert into funcionario(nome, sexo, dt_nasc, salario, cod_depto) values('Douglas Mateus Soares Cândido da Silva', 'H', '1997-09-23', 3333.33, 1);
+insert into funcionario(nome, sexo, dt_nasc, salario, cod_depto) values('Amanda Anisimova', 'M', '2001-08-31', 10000.00, 1);
 insert into funcionario(nome, sexo, dt_nasc, salario, cod_depto) values('Saint-Clair Cândido da Silva', 'H', '1971-09-07', 3333.33, 2);
 insert into funcionario(nome, sexo, dt_nasc, salario, cod_depto) values('Erivonete Soares de Lucena', 'M', '1969-05-10', 3333.33, 3);
 insert into funcionario(nome, sexo, dt_nasc, salario, cod_depto) values('Steven Paul Jobs', 'H', '1955-02-24', 100000.00, 4);
---
+++ tarefas/t01/tarefa01-q03.sql
@@ -0,0 +1,7 @@
+/* tarefa01-q03.sql */
+
+select f.nome, f.dt_nasc
+from funcionario f, departamento d
+where f.cod_depto = d.codigo and f.codigo not in (select cod_gerente from departamento d, funcionario f where f.cod_depto = d.codigo and d.cod_gerente = f.codigo) and f.codigo in (SELECT codigo FROM funcionario WHERE dt_nasc < (current_date - '21 years'::interval)::date);
+
+/* ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ */
