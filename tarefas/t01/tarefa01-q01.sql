f040186cbbec381534e28fd82eb4ec8a83dd206b
Douglas Cândido <douglasmateus1@hotmail.com>
2022-05-10 11:35:25 -0300

    enhancement: Minor changes. #1

--- tarefas/t01/tarefa01-q01.sql
+++ tarefas/t01/tarefa01-q01.sql
@@ -2,7 +2,7 @@
 
 select f.nome 
 from funcionario f, departamento d
-where (f.salario > (select max(salario) from funcionario, departamento where cod_depto = 5)) and f.cod_depto = d.codigo;
+where (f.salario > (select max(salario) from funcionario, departamento where cod_depto = 5 and funcionario.cod_depto = departamento.codigo)) and f.cod_depto = d.codigo;
 
 /* ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ */
 
