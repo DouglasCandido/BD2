9dc155bca38b884d74fe29dca10406b24ce03ae2
Douglas Cândido <douglasmateus1@hotmail.com>
2022-06-21 04:43:45 -0300

    Criada uma função que calcula a idade média dos funcionários de um departamento, resolve a questão 2.

---
+++ tarefas avulsas/Lista de Exercícios 1 - Funções e Procedimentos/2.sql
@@ -0,0 +1,31 @@
+/* 
+
+2. Faça uma função para calcular a média de idade por departamento
+
+*/
+
+insert into departamento(sigla, descricao) values ('DeptV', 'Departamento de Vendas');
+
+update funcionario set depto = 1 where codigo = 1;
+
+insert into funcionario(nome, sexo, dt_nasc, salario) values('Erivonete Soares de Lucena', 'M', '1969-05-10', 25000.00);
+
+update funcionario set depto = 1 where codigo = 2;
+
+insert into funcionario(nome, sexo, dt_nasc, salario) values('Saint-Clair Cândido da Silva', 'H', '1971-09-07', 50000.00);
+
+update funcionario set depto = 1 where codigo = 3;
+
+create or replace function media_idade_funcionarios_do_departamento(coddepto int)
+returns interval as $$
+declare idade_media interval;
+begin
+	select avg(age(CURRENT_DATE, f.dt_nasc)) into idade_media from funcionario f, departamento d where f.depto = d.codigo and d.codigo = coddepto; 
+	return idade_media;
+end;
+$$ language plpgsql;
+
+select media_idade_funcionarios_do_departamento(1);
+
+
+
